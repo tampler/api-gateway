b82ffed37e463b9ef54dd481ef60e50b674f609c
tampler <socnetfpga@gmail.com>
2022-07-08 22:01:51 +0300

    feat: moved SDK validators to token

--- internal/restserver/validators.go
+++ internal/restserver/validators.go
@@ -1,67 +1,24 @@
 package restserver
 
 import (
-	"fmt"
 	"net/http"
-	"strings"
 
 	"github.com/go-playground/validator/v10"
 	"github.com/labstack/echo/v4"
-	"github.com/neurodyne-web-services/nws-sdk-go/pkg/utils"
-	"github.com/neurodyne-web-services/nws-sdk-go/services/ec2"
-	"github.com/neurodyne-web-services/nws-sdk-go/services/session"
+	"github.com/neurodyne-web-services/api-gateway/internal/token"
 )
 
 const (
-	delimiter       = "::"
-	defaultSliceLen = 8
+	delimiter = "::"
 )
 
 type CustomValidator struct {
 	Validator *validator.Validate
 }
 
-func availableServices() []string {
-	out := make([]string, defaultSliceLen)
-
-	out = append(out, "EC2")
-	out = append(out, "Session")
-
-	return out
-}
-
-// availableResources - list resources from all available services
-func availableResources() []string {
-	out := make([]string, 2, 32)
-
-	// out = append(out, s3.AvailableResources()...)
-	out = append(out, ec2.AllResources...)
-	out = append(out, session.AllResources...)
-
-	return out
-}
-
-func commandValidator(command interface{}) error {
-	cmd := strings.Split(command.(string), delimiter)
-
-	if cmd[0] != "NWS" {
-		return fmt.Errorf("invalid provider specified. Default: NWS")
-	}
-
-	if !utils.Contains(availableServices(), cmd[1]) {
-		return fmt.Errorf("invalid service name specified")
-	}
-
-	if !utils.Contains(availableResources(), cmd[2]) {
-		return fmt.Errorf("invalid resource name specified")
-	}
-
-	return nil
-}
-
 func (cv *CustomValidator) Validate(cmd interface{}) error {
 	// Validate Command format
-	if err := commandValidator(cmd); err != nil {
+	if err := token.CommandValidator(cmd); err != nil {
 		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
 	}
 
---
+++ internal/token/validators.go
@@ -0,0 +1,53 @@
+package token
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/neurodyne-web-services/nws-sdk-go/pkg/utils"
+	"github.com/neurodyne-web-services/nws-sdk-go/services/ec2"
+	"github.com/neurodyne-web-services/nws-sdk-go/services/session"
+)
+
+const (
+	delimiter       = "::"
+	defaultSliceLen = 8
+)
+
+func AvailableServices() []string {
+	out := make([]string, defaultSliceLen)
+
+	out = append(out, "EC2")
+	out = append(out, "Session")
+
+	return out
+}
+
+// availableResources - list resources from all available services
+func AvailableResources() []string {
+	out := make([]string, 2, 32)
+
+	// out = append(out, s3.AvailableResources()...)
+	out = append(out, ec2.AllResources...)
+	out = append(out, session.AllResources...)
+
+	return out
+}
+
+func CommandValidator(command interface{}) error {
+	cmd := strings.Split(command.(string), delimiter)
+
+	if cmd[0] != "NWS" {
+		return fmt.Errorf("invalid provider specified. Default: NWS")
+	}
+
+	if !utils.Contains(AvailableServices(), cmd[1]) {
+		return fmt.Errorf("invalid service name specified")
+	}
+
+	if !utils.Contains(AvailableResources(), cmd[2]) {
+		return fmt.Errorf("invalid resource name specified")
+	}
+
+	return nil
+}
